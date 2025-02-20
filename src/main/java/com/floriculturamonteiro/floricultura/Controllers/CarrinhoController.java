f3eadd54d428c4c6a88ee2e9210f019e82c4f798
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-02-20 17:41:42 -0300

    refatoração de código

--- src/main/java/com/floriculturamonteiro/floricultura/Controllers/CarrinhoController.java
+++ src/main/java/com/floriculturamonteiro/floricultura/Controllers/CarrinhoController.java
@@ -9,6 +9,7 @@ import com.floriculturamonteiro.floricultura.service.CarrinhoService;
 import com.floriculturamonteiro.floricultura.service.CepService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/carrinho")
+@Scope("session") //um carrinho por sessão de usuário
 public class CarrinhoController {
 
     @Autowired
