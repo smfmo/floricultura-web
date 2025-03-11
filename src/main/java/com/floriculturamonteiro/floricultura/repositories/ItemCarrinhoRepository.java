853ad78129b1e87d522ff7598126f136efbbe606
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-03-11 10:42:52 -0300

    melhorias no Front-End, deixando as páginas mais uniforme

--- src/main/java/com/floriculturamonteiro/floricultura/repositories/ItemCarrinhoRepository.java
+++ src/main/java/com/floriculturamonteiro/floricultura/repositories/ItemCarrinhoRepository.java
@@ -2,9 +2,7 @@ package com.floriculturamonteiro.floricultura.repositories;
 
 import com.floriculturamonteiro.floricultura.model.ItemCarrinho;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long> {
     List<ItemCarrinho> findByCarrinhoId(Long carrinhoId);
