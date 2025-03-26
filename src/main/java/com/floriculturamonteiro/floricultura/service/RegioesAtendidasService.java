976f97182cf9a197b40337e7eaee671df5c8fe1d
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-03-26 11:14:39 -0300

    atualização dos valores de entrega.

--- src/main/java/com/floriculturamonteiro/floricultura/service/RegioesAtendidasService.java
+++ src/main/java/com/floriculturamonteiro/floricultura/service/RegioesAtendidasService.java
@@ -11,16 +11,16 @@ public class RegioesAtendidasService {
 
     private static final Map<String, BigDecimal> REGIOES_ATENDIDAS = new LinkedHashMap<>();
     static {
-        REGIOES_ATENDIDAS.put("Santa Maria", new BigDecimal("30"));
+        REGIOES_ATENDIDAS.put("Santa Maria", new BigDecimal("35"));
         REGIOES_ATENDIDAS.put("Céu Azul", new BigDecimal("20"));
         REGIOES_ATENDIDAS.put("Valparaíso 2", new BigDecimal("15"));
-        REGIOES_ATENDIDAS.put("Jardim Oriente", new BigDecimal("15"));
-        REGIOES_ATENDIDAS.put("Morada Nobre", new BigDecimal("15"));
+        REGIOES_ATENDIDAS.put("Jardim Oriente", new BigDecimal("10"));
+        REGIOES_ATENDIDAS.put("Morada Nobre", new BigDecimal("10"));
         REGIOES_ATENDIDAS.put("Setor A", new BigDecimal("15"));
-        REGIOES_ATENDIDAS.put("Setor B", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Setor C", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Setor D", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Setor E", new BigDecimal("0"));
+        REGIOES_ATENDIDAS.put("Setor B", new BigDecimal("15"));
+        REGIOES_ATENDIDAS.put("Setor C", new BigDecimal("15"));
+        REGIOES_ATENDIDAS.put("Setor D", new BigDecimal("15"));
+        REGIOES_ATENDIDAS.put("Setor E", new BigDecimal("15"));
         REGIOES_ATENDIDAS.put("Parque esplanada 1", new BigDecimal("20"));
         REGIOES_ATENDIDAS.put("Parque esplanada 2", new BigDecimal("20"));
         REGIOES_ATENDIDAS.put("Parque esplanada 3", new BigDecimal("15"));
@@ -33,10 +33,10 @@ public class RegioesAtendidasService {
         REGIOES_ATENDIDAS.put("Chácara Anhaguera 1", new BigDecimal("15"));
         REGIOES_ATENDIDAS.put("Chácara Anhaguera 2", new BigDecimal("15"));
         REGIOES_ATENDIDAS.put("Residencias Florais", new BigDecimal("20"));
-        REGIOES_ATENDIDAS.put("Mansões recreio Mossoró", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Parque Araguari 2", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Jardim Flamboyant", new BigDecimal("0"));
-        REGIOES_ATENDIDAS.put("Parque Nápolis", new BigDecimal("0"));
+        REGIOES_ATENDIDAS.put("Mansões recreio Mossoró", new BigDecimal("30"));
+        REGIOES_ATENDIDAS.put("Parque Araguari 2", new BigDecimal("30"));
+        REGIOES_ATENDIDAS.put("Jardim Flamboyant", new BigDecimal("30"));
+        REGIOES_ATENDIDAS.put("Parque Nápolis - Cidade Ocidental", new BigDecimal("30"));
         REGIOES_ATENDIDAS.put("Jardim Zuleika", new BigDecimal("25"));
         REGIOES_ATENDIDAS.put("Parque Marajó", new BigDecimal("25"));
         REGIOES_ATENDIDAS.put("Shopping Sul", new BigDecimal("25"));
