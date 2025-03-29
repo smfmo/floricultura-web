57a8679e4318d39baf01062589be04eeb1a83aa5
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-03-29 14:13:46 -0300

    refatoração e ajustes

--- src/main/java/com/floriculturamonteiro/floricultura/model/ItemCarrinho.java
+++ src/main/java/com/floriculturamonteiro/floricultura/model/ItemCarrinho.java
@@ -29,6 +29,9 @@ public class ItemCarrinho {
     @Column(name = "nome_produto")
     private String nomeProduto;
 
+    @Column(precision = 10, scale = 2, name = "valor_unitario")
+    private BigDecimal valorUnitario;
+
     //métodos getters e setters gerados pelo lombok
 
 }
