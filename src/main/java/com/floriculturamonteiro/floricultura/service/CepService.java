12637f1f39872bed1065ee511375dd1b105ec48c
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-01 14:42:45 -0300

    otimização de código

--- src/main/java/com/floriculturamonteiro/floricultura/service/CepService.java
+++ src/main/java/com/floriculturamonteiro/floricultura/service/CepService.java
@@ -1,24 +1,20 @@
 package com.floriculturamonteiro.floricultura.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-//refatorar para localizações estáticas - retirar API de Ceps
 
 @Service
-public class CepService {
+@RequiredArgsConstructor
+public class CepService { // não utilizável
 
     private final RegioesAtendidasService regiaoService;
-
-    @Autowired
-    public CepService(RegioesAtendidasService regiaoService) {
-        this.regiaoService = regiaoService;
-    }
 
     public boolean cepAtendido(String regiao){
         return regiaoService.regiaoAtendida(regiao);
     }
+
     public BigDecimal getPrecoEntrega(String regiao){
         return regiaoService.getPrecoEntrega(regiao);
     }
--- src/main/java/com/floriculturamonteiro/floricultura/service/ClienteService.java
+++ src/main/java/com/floriculturamonteiro/floricultura/service/ClienteService.java
@@ -2,18 +2,14 @@ package com.floriculturamonteiro.floricultura.service;
 
 import com.floriculturamonteiro.floricultura.model.Cliente;
 import com.floriculturamonteiro.floricultura.repositories.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
-
-    @Autowired
-    public ClienteService(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
-    }
 
     //metodo para salvar o cliente
     public void salvarCliente(Cliente cliente) {
