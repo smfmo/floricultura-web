dcf45a7843c5672753babcccfbead568b961b14c
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-04-14 12:19:02 -0300

    Feat: adição de pesquisa no catálogo

--- src/main/java/com/floriculturamonteiro/floricultura/repositories/FloresRepository.java
+++ src/main/java/com/floriculturamonteiro/floricultura/repositories/FloresRepository.java
@@ -2,10 +2,11 @@ package com.floriculturamonteiro.floricultura.repositories;
 
 import com.floriculturamonteiro.floricultura.model.Flores;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface FloresRepository extends JpaRepository<Flores, Long> {
+public interface FloresRepository extends JpaRepository<Flores, Long>, JpaSpecificationExecutor<Flores> {
     List<Flores> findByEmEstoqueTrue();
     List<Flores> findByEmEstoqueFalse();
 
