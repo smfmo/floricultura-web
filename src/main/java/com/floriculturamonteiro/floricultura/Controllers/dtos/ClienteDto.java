0eb60ca26ae1a3cb27dbda25e13ee698f37c6bb8
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-05-05 13:12:47 -0300

    Feat: implementação das validações de campo

--- src/main/java/com/floriculturamonteiro/floricultura/Controllers/dtos/ClienteDto.java
+++ src/main/java/com/floriculturamonteiro/floricultura/Controllers/dtos/ClienteDto.java
@@ -1,8 +1,18 @@
 package com.floriculturamonteiro.floricultura.Controllers.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record ClienteDto(
+        @NotBlank(message = "Campo obrigatório")
+                @Size(max = 255, min = 2, message = "Campo fora do tamanho padrão")
         String nome,
+        @Pattern(regexp = "^(\\(?\\d{2}\\)?[\\s-]?)?(\\d{4,5}[\\s-]?\\d{4})$",
+                message = "número de celular inválido Use (XX) XXXX-XXXX ou (XX) XXXXX-XXXX")
         String telefone,
+        @Email
         String email,
         EnderecoDto endereco) {
 }
