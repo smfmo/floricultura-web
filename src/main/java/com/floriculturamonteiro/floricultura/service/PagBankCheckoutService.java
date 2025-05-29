6dcdeecfee73e3f8f484fafcbda1051e3fbd9995
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-05-29 12:54:11 -0300

    integração concluida, Checkout funcionando

---
+++ src/main/java/com/floriculturamonteiro/floricultura/service/PagBankCheckoutService.java
@@ -0,0 +1,43 @@
+package com.floriculturamonteiro.floricultura.service;
+
+import com.floriculturamonteiro.floricultura.model.checkoutPagBank.checkout.Checkout;
+import com.floriculturamonteiro.floricultura.model.pedido.Carrinho;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class PagBankCheckoutService {
+
+    private final RestTemplate restTemplate;
+    private final PagBankMapperService pagBankMapperService;
+
+    @Value("${pagBank.api.url}")
+    private String pagBankUrl;
+
+    @Value("${pagBank.api.bearer_token}")
+    private String bearerToken;
+
+    public Checkout criarCheckout(Carrinho carrinho) {
+        Checkout checkoutRequest = pagBankMapperService.convertToPagBankCheckout(carrinho);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(bearerToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Checkout> request = new HttpEntity<>(checkoutRequest, headers);
+        ResponseEntity<Checkout> response = restTemplate
+                .postForEntity(
+                        pagBankUrl,
+                        request,
+                        Checkout.class);
+
+        return response.getBody();
+    }
+}
